e14e4bedc259263fd1599bfcb9264c9a7e788673
Bowei Du <bowei@google.com>
2017-12-22 16:05:57 -0800

    Move taskQueue to utils.PeriodicTaskQueue
    - Adds unit test for TaskQueue
    - Fixes minor violation of abstractions in controller_test.go

--- pkg/controller/utils.go
+++ pkg/controller/utils.go
@@ -19,17 +19,12 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
-	"time"
-
-	"github.com/golang/glog"
 
 	compute "google.golang.org/api/compute/v1"
 
 	api_v1 "k8s.io/api/core/v1"
 	extensions "k8s.io/api/extensions/v1beta1"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue"
 
 	"k8s.io/ingress-gce/pkg/annotations"
 	"k8s.io/ingress-gce/pkg/backends"
@@ -67,66 +62,6 @@ type ErrSvcAppProtosParsing struct {
 
 func (e ErrSvcAppProtosParsing) Error() string {
 	return fmt.Sprintf("could not parse %v annotation on Service %v/%v, err: %v", annotations.ServiceApplicationProtocolKey, e.svc.Namespace, e.svc.Name, e.origErr)
-}
-
-// taskQueue manages a work queue through an independent worker that
-// invokes the given sync function for every work item inserted.
-type taskQueue struct {
-	// queue is the work queue the worker polls
-	queue workqueue.RateLimitingInterface
-	// sync is called for each item in the queue
-	sync func(string) error
-	// workerDone is closed when the worker exits
-	workerDone chan struct{}
-}
-
-func (t *taskQueue) run(period time.Duration, stopCh <-chan struct{}) {
-	wait.Until(t.worker, period, stopCh)
-}
-
-// enqueue enqueues ns/name of the given api object in the task queue.
-func (t *taskQueue) enqueue(obj interface{}) {
-	key, err := keyFunc(obj)
-	if err != nil {
-		glog.Infof("Couldn't get key for object %+v: %v", obj, err)
-		return
-	}
-	t.queue.Add(key)
-}
-
-// worker processes work in the queue through sync.
-func (t *taskQueue) worker() {
-	for {
-		key, quit := t.queue.Get()
-		if quit {
-			close(t.workerDone)
-			return
-		}
-		glog.V(3).Infof("Syncing %v", key)
-		if err := t.sync(key.(string)); err != nil {
-			glog.Errorf("Requeuing %v, err %v", key, err)
-			t.queue.AddRateLimited(key)
-		} else {
-			t.queue.Forget(key)
-		}
-		t.queue.Done(key)
-	}
-}
-
-// shutdown shuts down the work queue and waits for the worker to ACK
-func (t *taskQueue) shutdown() {
-	t.queue.ShutDown()
-	<-t.workerDone
-}
-
-// NewTaskQueue creates a new task queue with the given sync function.
-// The sync function is called for every element inserted into the queue.
-func NewTaskQueue(syncFn func(string) error) *taskQueue {
-	return &taskQueue{
-		queue:      workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
-		sync:       syncFn,
-		workerDone: make(chan struct{}),
-	}
 }
 
 // compareLinks returns true if the 2 self links are equal.
---
+++ pkg/utils/taskqueue.go
@@ -0,0 +1,101 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"time"
+
+	"github.com/golang/glog"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+// TaskQueue is a rate limited operation queue.
+type TaskQueue interface {
+	Run(period time.Duration, stopCh <-chan struct{})
+	Enqueue(obj interface{})
+	Shutdown()
+}
+
+// PeriodicTaskQueue invokes the given sync function for every work item
+// inserted. If the sync() function results in an error, the item is put on
+// the work queue after a rate-limit.
+type PeriodicTaskQueue struct {
+	// keyFunc translates an object to a string-based key.
+	keyFunc func(obj interface{}) (string, error)
+	// queue is the work queue the worker polls.
+	queue workqueue.RateLimitingInterface
+	// sync is called for each item in the queue.
+	sync func(string) error
+	// workerDone is closed when the worker exits.
+	workerDone chan struct{}
+}
+
+// Run the task queue. This will block until the Shutdown() has been called.
+// TODO: seems redundant to both have stopCh and Shutdown().
+func (t *PeriodicTaskQueue) Run(period time.Duration, stopCh <-chan struct{}) {
+	wait.Until(t.worker, period, stopCh)
+}
+
+// Enqueue a key to the work queue.
+func (t *PeriodicTaskQueue) Enqueue(obj interface{}) {
+	key, err := t.keyFunc(obj)
+	if err != nil {
+		glog.Errorf("Couldn't get key for object %+v (type %T): %v", obj, obj, err)
+		return
+	}
+	glog.V(4).Infof("Enqueue key=%q", key)
+	t.queue.Add(key)
+}
+
+// Shutdown shuts down the work queue and waits for the worker to ACK
+func (t *PeriodicTaskQueue) Shutdown() {
+	glog.V(2).Infof("Shutdown")
+	t.queue.ShutDown()
+	<-t.workerDone
+}
+
+// worker processes work in the queue through sync.
+func (t *PeriodicTaskQueue) worker() {
+	for {
+		key, quit := t.queue.Get()
+		if quit {
+			close(t.workerDone)
+			return
+		}
+		glog.V(4).Infof("Syncing %v", key)
+		if err := t.sync(key.(string)); err != nil {
+			glog.Errorf("Requeuing %q due to error: %v", key, err)
+			t.queue.AddRateLimited(key)
+		} else {
+			t.queue.Forget(key)
+		}
+		t.queue.Done(key)
+	}
+}
+
+// NewPeriodicTaskQueue creates a new task queue with the given sync function.
+// The sync function is called for every element inserted into the queue.
+func NewPeriodicTaskQueue(syncFn func(string) error) *PeriodicTaskQueue {
+	return &PeriodicTaskQueue{
+		keyFunc:    cache.DeletionHandlingMetaNamespaceKeyFunc,
+		queue:      workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter()),
+		sync:       syncFn,
+		workerDone: make(chan struct{}),
+	}
+}
