06ddd376274901c5fa9aac5f1f4c80f7f51465ed
Zihong Zheng <zihongz@google.com>
2018-03-19 21:49:24 -0700

    Define empty ServiceExtension API

--- pkg/serviceextension/crd.go
+++ pkg/serviceextension/crd.go
@@ -27,6 +27,8 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
+
+	apisserviceextension "k8s.io/ingress-gce/pkg/apis/serviceextension"
 )
 
 const (
@@ -38,9 +40,9 @@ const (
 
 func getCRDSpec() *apiextensionsv1beta1.CustomResourceDefinition {
 	return &apiextensionsv1beta1.CustomResourceDefinition{
-		ObjectMeta: metav1.ObjectMeta{Name: "serviceextensions.cloud.google.com"},
+		ObjectMeta: metav1.ObjectMeta{Name: "serviceextensions" + "." + apisserviceextension.GroupName},
 		Spec: apiextensionsv1beta1.CustomResourceDefinitionSpec{
-			Group:   "cloud.google.com",
+			Group:   apisserviceextension.GroupName,
 			Version: "v1alpha1",
 			Scope:   apiextensionsv1beta1.NamespaceScoped,
 			Names: apiextensionsv1beta1.CustomResourceDefinitionNames{
