08529465391c5319a2a137b3d7160b123acec74d
Rohit Ramkumar <rramkumar@google.com>
2018-07-25 11:29:35 -0700

    Rename context's hcLock to lock

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -49,7 +49,8 @@ type ControllerContext struct {
 	EndpointInformer      cache.SharedIndexInformer
 
 	healthChecks map[string]func() error
-	hcLock       sync.Mutex
+
+	lock sync.Mutex
 
 	// Map of namespace => record.EventRecorder.
 	recorders map[string]record.EventRecorder
@@ -136,8 +137,8 @@ func (ctx *ControllerContext) Recorder(ns string) record.EventRecorder {
 
 // AddHealthCheck registers function to be called for healthchecking.
 func (ctx *ControllerContext) AddHealthCheck(id string, hc func() error) {
-	ctx.hcLock.Lock()
-	defer ctx.hcLock.Unlock()
+	ctx.lock.Lock()
+	defer ctx.lock.Unlock()
 
 	ctx.healthChecks[id] = hc
 }
@@ -147,8 +148,8 @@ type HealthCheckResults map[string]error
 
 // HealthCheck runs all registered healthcheck functions.
 func (ctx *ControllerContext) HealthCheck() HealthCheckResults {
-	ctx.hcLock.Lock()
-	defer ctx.hcLock.Unlock()
+	ctx.lock.Lock()
+	defer ctx.lock.Unlock()
 
 	healthChecks := make(map[string]error)
 	for component, f := range ctx.healthChecks {
