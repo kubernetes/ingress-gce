d583dcd9c3548c674c84d9b581395d887a179e37
Rohit Ramkumar <rramkumar@google.com>
2018-08-29 13:12:57 -0700

    Remove JSONMergePatch util

--- pkg/utils/patch.go
+++ pkg/utils/patch.go
@@ -17,9 +17,6 @@ import (
 	"encoding/json"
 	"fmt"
 
-	api_v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/meta"
-	"k8s.io/apimachinery/pkg/util/jsonmergepatch"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
 )
 
@@ -42,50 +39,4 @@ func StrategicMergePatchBytes(old, cur, refStruct interface{}) ([]byte, error) {
 	}
 
 	return patchBytes, nil
-}
-
-// JSONMergePatchBytes returns a patch between the old and new object using a JSON merge patch.
-func JSONMergePatchBytes(old, cur interface{}) ([]byte, error) {
-	oldBytes, err := json.Marshal(old)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal old object: %v", err)
-	}
-
-	newBytes, err := json.Marshal(cur)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal new object: %v", err)
-	}
-
-	lastAppliedBytes, err := lastAppliedConfigurationBytes(old)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get last applied config for old object: %v", err)
-	}
-
-	patchBytes, err := jsonmergepatch.CreateThreeWayJSONMergePatch(lastAppliedBytes, newBytes, oldBytes)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create patch: %v", err)
-	}
-
-	return patchBytes, nil
-}
-
-// lastAppliedConfigurationBytes returns the bytes of the original configuration of an object
-// from the annotation, or nil if no annotation is found. This is needed for the 3-way JSON merge patch.
-func lastAppliedConfigurationBytes(obj interface{}) ([]byte, error) {
-	accessor, err := meta.Accessor(obj)
-	if err != nil {
-		return nil, err
-	}
-
-	annotations := accessor.GetAnnotations()
-	if annotations == nil {
-		return nil, nil
-	}
-
-	original, ok := annotations[api_v1.LastAppliedConfigAnnotation]
-	if !ok {
-		return nil, nil
-	}
-
-	return []byte(original), nil
 }
