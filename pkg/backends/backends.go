6bd6ecd7ae0a6f3320c84983aba2d802aa2d5207
Rohit Ramkumar <rramkumar@google.com>
2018-09-04 11:26:12 -0700

    Revert "Refactor some uses of snapshotter.Add() to use bool rather than real object"

--- pkg/backends/backends.go
+++ pkg/backends/backends.go
@@ -99,7 +99,7 @@ func (b *Backends) Create(sp utils.ServicePort, hcLink string) (*composite.Backe
 	if err := composite.CreateBackendService(be, b.cloud); err != nil {
 		return nil, err
 	}
-	b.snapshotter.Add(name, true)
+	b.snapshotter.Add(name, be)
 	// Note: We need to perform a GCE call to re-fetch the object we just created
 	// so that the "Fingerprint" field is filled in. This is needed to update the
 	// object without error.
@@ -113,7 +113,7 @@ func (b *Backends) Update(be *composite.BackendService) error {
 	if err := composite.UpdateBackendService(be, b.cloud); err != nil {
 		return err
 	}
-	b.snapshotter.Add(be.Name, true)
+	b.snapshotter.Add(be.Name, be)
 	return nil
 }
 
@@ -133,7 +133,7 @@ func (b *Backends) Get(name string, version meta.Version) (*composite.BackendSer
 			return nil, err
 		}
 	}
-	b.snapshotter.Add(name, true)
+	b.snapshotter.Add(name, be)
 	return be, nil
 }
 
@@ -193,8 +193,8 @@ func (b *Backends) List() ([]interface{}, error) {
 		return nil, err
 	}
 	var ret []interface{}
-	for _, _ = range backends {
-		ret = append(ret, true)
+	for _, x := range backends {
+		ret = append(ret, x)
 	}
 	return ret, nil
 }
