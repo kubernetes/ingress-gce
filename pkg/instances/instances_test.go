e28f9b8ca85be0cd08de496ea5b2aad6f0b95808
Rohit Ramkumar <rramkumar@google.com>
2018-12-17 09:50:13 -0800

    Revert "Remove unused named ports from instance group's"
    This reverts commit fbe88aa88b961ab683af8486da6e040cbd75584c.

--- pkg/instances/instances_test.go
+++ pkg/instances/instances_test.go
@@ -86,39 +86,43 @@ func TestSetNamedPorts(t *testing.T) {
 	f := NewFakeInstanceGroups(sets.NewString([]string{"ig"}...), defaultNamer)
 	pool := newNodePool(f, defaultZone)
 
-	// Note: each test case is dependent on the previous.
 	testCases := []struct {
-		desc          string
+		activePorts   []int64
 		expectedPorts []int64
 	}{
 		{
-			desc:          "Set single port",
-			expectedPorts: []int64{80},
+			// Verify setting a port works as expected.
+			[]int64{80},
+			[]int64{80},
 		},
 		{
-			desc:          "Two new ports + existing port",
-			expectedPorts: []int64{80, 81, 82},
+			// Utilizing multiple new ports
+			[]int64{81, 82},
+			[]int64{80, 81, 82},
 		},
 		{
-			desc:          "Utilize all existing ports",
-			expectedPorts: []int64{80, 81, 82},
+			// Utilizing existing ports
+			[]int64{80, 82},
+			[]int64{80, 81, 82},
 		},
 		{
-			desc:          "Two new ports + remove unused port",
-			expectedPorts: []int64{81, 82, 83, 84},
+			// Utilizing a new port and an old port
+			[]int64{80, 83},
+			[]int64{80, 81, 82, 83},
 		},
+		// TODO: Add tests to remove named ports when we support that.
 	}
 	for _, test := range testCases {
-		igs, err := pool.EnsureInstanceGroupsAndPorts("ig", test.expectedPorts)
+		igs, err := pool.EnsureInstanceGroupsAndPorts("ig", test.activePorts)
 		if err != nil {
-			t.Fatalf("unexpected error in setting ports %v to instance group: %s", test.expectedPorts, err)
+			t.Fatalf("unexpected error in setting ports %v to instance group: %s", test.activePorts, err)
 		}
 		if len(igs) != 1 {
 			t.Fatalf("expected a single instance group, got: %v", igs)
 		}
 		actualPorts := igs[0].NamedPorts
 		if len(actualPorts) != len(test.expectedPorts) {
-			t.Fatalf("unexpected number of named ports on instance group. expected: %v, got: %v", len(test.expectedPorts), len(actualPorts))
+			t.Fatalf("unexpected named ports on instance group. expected: %v, got: %v", test.expectedPorts, actualPorts)
 		}
 		for i, p := range igs[0].NamedPorts {
 			if p.Port != test.expectedPorts[i] {
