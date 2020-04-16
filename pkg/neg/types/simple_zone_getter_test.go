76f4edaae02abd5448a5a93b5257049830e9d5f7
Minhan Xia <mixia@google.com>
2020-04-16 14:42:47 -0700

    allow local zone override for NonGCP NEG

---
+++ pkg/neg/types/simple_zone_getter_test.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package types
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSimpleZoneGetter(t *testing.T) {
+	zone := "foo"
+	zoneGetter := NewSimpleZoneGetter(zone)
+	ret, err := zoneGetter.ListZones()
+	if err != nil {
+		t.Errorf("expect err = nil, but got %v", err)
+	}
+	expectZones := []string{zone}
+	if !reflect.DeepEqual(expectZones, ret) {
+		t.Errorf("expect list zones = %v, but got %v", expectZones, ret)
+	}
+
+	validateGetZoneForNode := func(node string) {
+		retZone, err := zoneGetter.GetZoneForNode(node)
+		if err != nil {
+			t.Errorf("expect err = nil, but got %v", err)
+		}
+
+		if retZone != zone {
+			t.Errorf("expect zone = %q, but got %q", zone, retZone)
+		}
+	}
+
+	validateGetZoneForNode("foo-node")
+	validateGetZoneForNode("bar-node")
+}
