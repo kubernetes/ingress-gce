ae617f2ec2547a74aeff1a930842043124d86e22
Swetha Repakula <srepakula@google.com>
2020-08-14 12:22:44 -0700

    Update codegen for ServiceNetworkEndpointGroup
     - Update openapi generation tags
     - change NegObjectReference.Id type to string

--- pkg/apis/svcneg/v1beta1/zz_generated.openapi.go
+++ pkg/apis/svcneg/v1beta1/zz_generated.openapi.go
@@ -29,8 +29,101 @@ import (
 
 func GetOpenAPIDefinitions(ref common.ReferenceCallback) map[string]common.OpenAPIDefinition {
 	return map[string]common.OpenAPIDefinition{
-		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.ServiceNetworkEndpointGroup":     schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroup(ref),
-		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.ServiceNetworkEndpointGroupSpec": schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroupSpec(ref),
+		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.Condition":                         schema_pkg_apis_svcneg_v1beta1_Condition(ref),
+		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.NegObjectReference":                schema_pkg_apis_svcneg_v1beta1_NegObjectReference(ref),
+		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.ServiceNetworkEndpointGroup":       schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroup(ref),
+		"k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.ServiceNetworkEndpointGroupStatus": schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroupStatus(ref),
+	}
+}
+
+func schema_pkg_apis_svcneg_v1beta1_Condition(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "NegCondition contains details for the current condition of this NEG.",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"type": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Type is the type of the condition.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"status": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Status of the condition, one of True, False, Unknown.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"observedGeneration": {
+						SchemaProps: spec.SchemaProps{
+							Description: "ObservedGeneration will not be set for ServiceNetworkEndpointGroup as the spec is empty.",
+							Type:        []string{"integer"},
+							Format:      "int64",
+						},
+					},
+					"lastTransitionTime": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Last time the condition transitioned from one status to another.",
+							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Time"),
+						},
+					},
+					"reason": {
+						SchemaProps: spec.SchemaProps{
+							Description: "The reason for the condition's last transition",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"message": {
+						SchemaProps: spec.SchemaProps{
+							Description: "A human readable message indicating details about the transition. This field may be empty.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+				Required: []string{"type", "status", "reason", "message"},
+			},
+		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/apis/meta/v1.Time"},
+	}
+}
+
+func schema_pkg_apis_svcneg_v1beta1_NegObjectReference(ref common.ReferenceCallback) common.OpenAPIDefinition {
+	return common.OpenAPIDefinition{
+		Schema: spec.Schema{
+			SchemaProps: spec.SchemaProps{
+				Description: "NegObjectReference is the object reference to the NEG resource in GCE",
+				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"id": {
+						SchemaProps: spec.SchemaProps{
+							Description: "The unique identifier for the NEG resource in GCE API.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"selfLink": {
+						SchemaProps: spec.SchemaProps{
+							Description: "SelfLink is the GCE Server-defined fully-qualified URL for the GCE NEG resource",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+					"networkEndpointType": {
+						SchemaProps: spec.SchemaProps{
+							Description: "NetworkEndpointType: Type of network endpoints in this network endpoint group.",
+							Type:        []string{"string"},
+							Format:      "",
+						},
+					},
+				},
+			},
+		},
 	}
 }
 
@@ -77,13 +170,64 @@ func schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroup(ref common.Refer
 	}
 }
 
-func schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroupSpec(ref common.ReferenceCallback) common.OpenAPIDefinition {
+func schema_pkg_apis_svcneg_v1beta1_ServiceNetworkEndpointGroupStatus(ref common.ReferenceCallback) common.OpenAPIDefinition {
 	return common.OpenAPIDefinition{
 		Schema: spec.Schema{
 			SchemaProps: spec.SchemaProps{
-				Description: "ServiceNetworkEndpointGroupSpec is the spec for a ServiceNetworkEndpointGroup resource",
+				Description: "ServiceNetworkEndpointGroupStatus is the status for a ServiceNetworkEndpointGroup resource",
 				Type:        []string{"object"},
+				Properties: map[string]spec.Schema{
+					"networkEndpointGroups": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-map-keys": []interface{}{
+									"id",
+								},
+								"x-kubernetes-list-type": "map",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Type: []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.NegObjectReference"),
+									},
+								},
+							},
+						},
+					},
+					"conditions": {
+						VendorExtensible: spec.VendorExtensible{
+							Extensions: spec.Extensions{
+								"x-kubernetes-list-map-keys": []interface{}{
+									"type",
+								},
+								"x-kubernetes-list-type": "map",
+							},
+						},
+						SchemaProps: spec.SchemaProps{
+							Description: "Last time the NEG syncer syncs associated NEGs.",
+							Type:        []string{"array"},
+							Items: &spec.SchemaOrArray{
+								Schema: &spec.Schema{
+									SchemaProps: spec.SchemaProps{
+										Ref: ref("k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.Condition"),
+									},
+								},
+							},
+						},
+					},
+					"lastSyncTime": {
+						SchemaProps: spec.SchemaProps{
+							Description: "Last time the NEG syncer syncs associated NEGs.",
+							Ref:         ref("k8s.io/apimachinery/pkg/apis/meta/v1.Time"),
+						},
+					},
+				},
 			},
 		},
+		Dependencies: []string{
+			"k8s.io/apimachinery/pkg/apis/meta/v1.Time", "k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.Condition", "k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1.NegObjectReference"},
 	}
 }
