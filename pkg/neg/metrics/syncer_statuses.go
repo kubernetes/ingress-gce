397e82f1a6626e89900460c535c3a3a691f45f1d
David Cheung <cheungdavid@google.com>
2023-01-26 20:15:23 +0000

    add syncer status

---
+++ pkg/neg/metrics/syncer_statuses.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2020 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+type syncerStatus string
+
+func (s syncerStatus) String() string {
+	return string(s)
+}
+
+const (
+	syncerEPCountsDiffer         = syncerStatus("SyncerEPCountsDiffer")
+	syncerEPMissingNodeName      = syncerStatus("SyncerEPMissingNodeName")
+	syncerEPMissingZone          = syncerStatus("SyncerEPMissingZone")
+	syncerInvalidEPAttach        = syncerStatus("SyncerInvalidEPAttach")
+	syncerInvalidEPDetach        = syncerStatus("SyncerInvalidEPDetach")
+	syncerEPSEndpointCountZero   = syncerStatus("SyncerEPSEndpointCountZero")
+	syncerEPCalculationCountZero = syncerStatus("SyncerEPCalculationCountZero")
+	syncerNegNotFound            = syncerStatus("SyncerNegNotFound")
+	syncerCurrentEPNotFound      = syncerStatus("SyncerCurrentEPNotFound")
+	syncerEPSNotFound            = syncerStatus("SyncerEPSNotFound")
+	syncerNodeNotFound           = syncerStatus("SyncerNodeNotFound")
+	syncerOtherError             = syncerStatus("SyncerOtherError")
+	syncerInSuccess              = syncerStatus("SyncerInSuccess")
+)
+
+func getSyncerStatus(err error) syncerStatus {
+	switch err {
+	case ErrEPCountsDiffer:
+		return syncerEPCountsDiffer
+	case ErrEPMissingNodeName:
+		return syncerEPMissingNodeName
+	case ErrEPMissingZone:
+		return syncerEPMissingZone
+	case ErrInvalidEPAttach:
+		return syncerInvalidEPAttach
+	case ErrInvalidEPDetach:
+		return syncerInvalidEPDetach
+	case ErrEPSEndpointCountZero:
+		return syncerEPSEndpointCountZero
+	case ErrEPCalculationCountZero:
+		return syncerEPCalculationCountZero
+	case ErrNegNotFound:
+		return syncerNegNotFound
+	case ErrCurrentEPNotFound:
+		return syncerCurrentEPNotFound
+	case ErrEPSNotFound:
+		return syncerEPSNotFound
+	case ErrNodeNotFound:
+		return syncerNodeNotFound
+	default:
+		return syncerOtherError
+	}
+}
