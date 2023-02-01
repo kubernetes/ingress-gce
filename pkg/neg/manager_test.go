3e6ac45ed4167ed30a2e1e374850d3e90fc05e6c
David Cheung <cheungdavid@google.com>
2023-02-01 23:18:23 +0000

    initialize metrics collector

--- pkg/neg/manager_test.go
+++ pkg/neg/manager_test.go
@@ -42,6 +42,7 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/tools/record"
 	negv1beta1 "k8s.io/ingress-gce/pkg/apis/svcneg/v1beta1"
+	"k8s.io/ingress-gce/pkg/neg/metrics"
 	"k8s.io/ingress-gce/pkg/neg/types"
 	negtypes "k8s.io/ingress-gce/pkg/neg/types"
 	svcnegclient "k8s.io/ingress-gce/pkg/svcneg/client/clientset/versioned"
@@ -91,6 +92,7 @@ func NewTestSyncerManager(kubeClient kubernetes.Interface) (*syncerManager, *gce
 		testContext.EndpointSliceInformer.GetIndexer(),
 		testContext.NodeInformer.GetIndexer(),
 		testContext.SvcNegInformer.GetIndexer(),
+		metrics.FakeSyncerMetrics(),
 		false, //enableNonGcpMode
 		false, //enableEndpointSlices
 		klog.TODO(),
