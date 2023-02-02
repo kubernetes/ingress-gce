fffde4e983789081ec759b85bf1f93d005a21d9d
David Cheung <cheungdavid@google.com>
2023-02-02 00:46:18 +0000

    create syncer status metrics

--- pkg/neg/metrics/neg_metrics_collector.go
+++ pkg/neg/metrics/neg_metrics_collector.go
@@ -21,6 +21,7 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/apimachinery/pkg/util/wait"
 	negtypes "k8s.io/ingress-gce/pkg/neg/types"
 	"k8s.io/klog/v2"
 )
@@ -28,9 +29,22 @@ import (
 const (
 	syncResultLabel = "result"
 	syncResultKey   = "sync_result"
+
+	syncerStatusLabel = "status"
+	syncerStatusKey   = "syncer_status"
 )
 
 var (
+	// syncerSyncerStatus tracks the count of syncer in different statuses
+	syncerSyncerStatus = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Subsystem: negControllerSubsystem,
+			Name:      syncerStatusKey,
+			Help:      "Current count of syncers in each status",
+		},
+		[]string{syncerStatusLabel},
+	)
+
 	// syncerSyncResult tracks the count for each sync result
 	syncerSyncResult = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
@@ -47,6 +61,8 @@ type SyncerMetricsCollector interface {
 }
 
 type SyncerMetrics struct {
+	// syncerStatusmap tracks the status of each syncer
+	syncerStatusMap map[negtypes.NegSyncerKey]string
 	// mu avoid race conditions and ensure correctness of metrics
 	mu sync.Mutex
 	// duration between metrics exports
@@ -58,6 +74,7 @@ type SyncerMetrics struct {
 // NewNEGMetricsCollector initializes SyncerMetrics and starts a go routine to compute and export metrics periodically.
 func NewNegMetricsCollector(exportInterval time.Duration, logger klog.Logger) *SyncerMetrics {
 	return &SyncerMetrics{
+		syncerStatusMap: make(map[negtypes.NegSyncerKey]string),
 		metricsInterval: exportInterval,
 		logger:          logger.WithName("NegMetricsCollector"),
 	}
@@ -70,9 +87,66 @@ func FakeSyncerMetrics() *SyncerMetrics {
 
 func RegisterSyncerMetrics() {
 	prometheus.MustRegister(syncerSyncResult)
+	prometheus.MustRegister(syncerSyncerStatus)
+}
+
+func (sm *SyncerMetrics) Run(stopCh <-chan struct{}) {
+	sm.logger.V(3).Info("Syncer Metrics initialized.", "exportInterval", sm.metricsInterval)
+	// Compute and export metrics periodically.
+	go func() {
+		time.Sleep(sm.metricsInterval)
+		wait.Until(sm.export, sm.metricsInterval, stopCh)
+	}()
+	<-stopCh
+}
+
+// export exports syncer metrics.
+func (sm *SyncerMetrics) export() {
+	statusCount, syncerCount := sm.computeSyncerStatusMetrics()
+	sm.logger.V(3).Info("Exporting syncer status metrics.", "Syncer count", syncerCount)
+	for syncerStatus, count := range statusCount {
+		syncerSyncerStatus.WithLabelValues(syncerStatus).Set(float64(count))
+	}
 }
 
 // UpdateSyncer updates the count of sync results based on the result/error of sync
 func (sm *SyncerMetrics) UpdateSyncer(key negtypes.NegSyncerKey, syncResult *negtypes.NegSyncResult) {
 	syncerSyncResult.WithLabelValues(syncResult.Result).Inc()
+	syncerStatus := negtypes.GetSyncerStatus(syncResult.Result)
+
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	if sm.syncerStatusMap == nil {
+		sm.syncerStatusMap = make(map[negtypes.NegSyncerKey]string)
+		sm.logger.V(3).Info("Syncer Metrics failed to initialize correctly, reinitializing syncerStatusMap: %v", sm.syncerStatusMap)
+	}
+	sm.syncerStatusMap[key] = syncerStatus
+}
+
+func (sm *SyncerMetrics) computeSyncerStatusMetrics() (map[string]int, int) {
+	sm.mu.Lock()
+	defer sm.mu.Unlock()
+	sm.logger.V(3).Info("computing syncer status metrics")
+
+	statusCount := map[string]int{
+		negtypes.SyncerEPCountsDiffer:         0,
+		negtypes.SyncerEPMissingNodeName:      0,
+		negtypes.SyncerNodeNotFound:           0,
+		negtypes.SyncerEPMissingZone:          0,
+		negtypes.SyncerEPSEndpointCountZero:   0,
+		negtypes.SyncerEPCalculationCountZero: 0,
+		negtypes.SyncerInvalidEPAttach:        0,
+		negtypes.SyncerInvalidEPDetach:        0,
+		negtypes.SyncerNegNotFound:            0,
+		negtypes.SyncerCurrentEPNotFound:      0,
+		negtypes.SyncerEPSNotFound:            0,
+		negtypes.SyncerOtherError:             0,
+		negtypes.SyncerSuccess:                0,
+	}
+	syncerCount := 0
+	for _, syncerStatus := range sm.syncerStatusMap {
+		statusCount[syncerStatus] += 1
+		syncerCount += 1
+	}
+	return statusCount, syncerCount
 }
