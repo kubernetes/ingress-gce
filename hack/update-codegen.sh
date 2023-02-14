e0ddc87a1d0d929cd60393c6f76feff47e7c6782
Ruixian Song <songrx@google.com>
2023-02-14 10:42:47 -0800

    Generate code for NEGControllerConfig CRD

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -150,3 +150,17 @@ ${OPENAPI_PKG}/openapi-gen \
   --input-dirs k8s.io/ingress-gce/pkg/apis/ingparams/v1beta1 \
   --output-package k8s.io/ingress-gce/pkg/apis/ingparams/v1beta1 \
   --go-header-file ${SCRIPT_ROOT}/boilerplate.go.txt
+
+echo "Performing code generation for NEGControllerConfig CRD"
+${CODEGEN_PKG}/generate-groups.sh \
+  "deepcopy,client,informer,lister" \
+  k8s.io/ingress-gce/pkg/negcontrollerconfig/client k8s.io/ingress-gce/pkg/apis \
+  "negcontrollerconfig:v1beta1" \
+  --go-header-file ${SCRIPT_ROOT}/boilerplate.go.txt
+
+echo "Generating openapi for NEGControllerConfig v1beta1"
+${OPENAPI_PKG}/openapi-gen \
+  --output-file-base zz_generated.openapi \
+  --input-dirs k8s.io/ingress-gce/pkg/apis/negcontrollerconfig/v1beta1 \
+  --output-package k8s.io/ingress-gce/pkg/apis/negcontrollerconfig/v1beta1 \
+  --go-header-file ${SCRIPT_ROOT}/boilerplate.go.txt
