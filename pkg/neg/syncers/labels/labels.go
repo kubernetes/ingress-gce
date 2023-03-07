d31d97e6ed277c16f424ee31569f0ef76456136a
Ruixian Song <songrx@google.com>
2023-03-06 17:54:36 -0800

    Add getPodLabelMap function for label propagation

--- pkg/neg/syncers/labels/labels.go
+++ pkg/neg/syncers/labels/labels.go
@@ -19,7 +19,11 @@ package labels
 import (
 	"errors"
 
+	"fmt"
+
+	v1 "k8s.io/api/core/v1"
 	negtypes "k8s.io/ingress-gce/pkg/neg/types"
+	"k8s.io/ingress-gce/pkg/utils"
 )
 
 // PodLabelPropagationConfig contains a list of configurations for labels to be propagated to GCE network endpoints.
@@ -44,3 +48,55 @@ var (
 	ErrLabelTruncated        = errors.New("label is truncated")
 	ErrLabelTruncationFailed = errors.New("failed to truncate label")
 )
+
+// minLabelLength defines the minimum space left for the label value.
+const minLabelLength = 5
+
+// GetPodLabelMap will return the label map extracted from a pod according to PodLabelPropagationConfig.
+// The returned map has the pod label key as key and label value as value.
+// This function will raise an error if pod label truncation happens or truncation fails.
+func GetPodLabelMap(pod *v1.Pod, lpConfig PodLabelPropagationConfig) (PodLabelMap, error) {
+	labelMap := PodLabelMap{}
+	var errs []error
+	for _, label := range lpConfig.Labels {
+		val, ok := pod.Labels[label.Key]
+		if ok {
+			lpKey := label.Key
+			if label.ShortKey != "" {
+				lpKey = label.ShortKey
+			}
+			labelVal, err := truncatePodLabel(lpKey, val, label.MaxLabelSizeBytes)
+			if err != nil {
+				errs = append(errs, err)
+			}
+
+			// Add the label to the map only if the truncation result is valid
+			if err == nil || errors.Is(err, ErrLabelTruncated) {
+				labelMap[lpKey] = labelVal
+			}
+		}
+	}
+	if len(errs) != 0 {
+		return labelMap, utils.JoinErrs(errs)
+	}
+	return labelMap, nil
+}
+
+// truncatePodLabel calculates the potentially truncated label value to ensure that len(key) + len(label) <= maxTotalSize.
+// It will return:
+//
+//	A string which is the truncated label value.
+//	ErrLabelTruncated if truncation happens or ErrLabelTruncationFailed if truncation fails.
+func truncatePodLabel(key, label string, maxTotalSize int) (string, error) {
+	keyBytes := []byte(key)
+	labelBytes := []byte(label)
+	totalLen := len(keyBytes) + len(labelBytes)
+	if totalLen <= maxTotalSize {
+		return label, nil
+	}
+	if len(keyBytes)+minLabelLength > maxTotalSize {
+		return "", fmt.Errorf("%w: `%s:%s` truncation failed because the key exceeded the limit, length: %d, limit %d", ErrLabelTruncationFailed, key, label, len(keyBytes)+minLabelLength, maxTotalSize)
+	}
+	truncatedVal := string(labelBytes[:maxTotalSize-len(keyBytes)])
+	return truncatedVal, fmt.Errorf("%w: `%s:%s` is truncated to `%s:%s` because the total length exceeded the limit, length: %d, limit: %d", ErrLabelTruncated, key, label, key, truncatedVal, len(key)+len(label), maxTotalSize)
+}
