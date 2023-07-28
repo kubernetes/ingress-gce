3d68f6a3accf33c8315e51a81a7e0c230f80c95e
Ruixian Song <songrx@google.com>
2023-07-28 13:16:21 -0700

    Add metrics for empty string security policy and nil security policy

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -638,6 +638,9 @@ func initializeCounts() (map[feature]int, map[feature]int) {
 			neg:                       0,
 			cloudCDN:                  0,
 			cloudArmor:                0,
+			cloudArmorSet:             0,
+			cloudArmorEmpty:           0,
+			cloudArmorNil:             0,
 			cloudIAP:                  0,
 			backendTimeout:            0,
 			backendConnectionDraining: 0,
@@ -668,6 +671,9 @@ func isServiceFeature(ftr feature) bool {
 		servicePort:         true,
 		externalServicePort: true,
 		internalServicePort: true,
+		cloudArmorEmpty:     true,
+		cloudArmorNil:       true,
+		cloudArmorSet:       true,
 	}
 	return serviceFeatures[ftr]
 }
