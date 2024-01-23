8aa9aacfa7753497e0110c38f30e5187dc2232a3
Slavik Panasovets <slavik@google.com>
2024-01-23 20:04:30 +0000

    Create proper firewall for Regional XLB Ingress
    - We need to create firewall to allow traffic from proxy-subnet to nodes

---
+++ pkg/loadbalancers/features/l7rxlb.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package features
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-cloud-provider/pkg/cloud/filter"
+	"k8s.io/cloud-provider-gcp/providers/gce"
+	"k8s.io/klog/v2"
+)
+
+// RXLBSubnetSourceRange gets Subnet source range for RXLB.
+func RXLBSubnetSourceRange(cloud *gce.Cloud, region string) (string, error) {
+	subnets, err := cloud.Compute().BetaSubnetworks().List(context.Background(), region, filter.None)
+	if err != nil {
+		return "", fmt.Errorf("error obtaining subnets for region %s, %v", region, err)
+	}
+
+	for _, subnet := range subnets {
+		sameNetwork, err := isSameNetwork(subnet.Network, cloud.NetworkURL())
+		if err != nil {
+			return "", fmt.Errorf("error comparing subnets: %v", err)
+		}
+		if subnet.Role == "ACTIVE" && (subnet.Purpose == "REGIONAL_MANAGED_PROXY") && sameNetwork {
+			klog.V(3).Infof("Found L7-RXLB Subnet %s - %s", subnet.Name, subnet.IpCidrRange)
+			return subnet.IpCidrRange, nil
+		}
+	}
+	return "", ErrSubnetNotFound
+}
