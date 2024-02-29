88ab11d11bec0639f42411f52c93afc9968b20f5
Slavik Panasovets <slavik@google.com>
2024-02-29 22:01:25 +0000

    Don't return error if ingress already do not exist

--- pkg/e2e/helpers.go
+++ pkg/e2e/helpers.go
@@ -273,7 +273,7 @@ func performWhiteboxTests(s *Sandbox, ing *networkingv1.Ingress, fc *frontendcon
 // resources associated with it to be deleted.
 func WaitForIngressDeletion(ctx context.Context, g *fuzz.GCLB, s *Sandbox, ing *networkingv1.Ingress, options *fuzz.GCLBDeleteOptions) error {
 	crud := adapter.IngressCRUD{C: s.f.Clientset}
-	if err := crud.Delete(ing.Namespace, ing.Name); err != nil {
+	if err := crud.Delete(ing.Namespace, ing.Name); err != nil && !apierrors.IsNotFound(err) {
 		return fmt.Errorf("delete(%q) = %v, want nil", ing.Name, err)
 	}
 	klog.Infof("Waiting for GCLB resources to be deleted (%s/%s), IngressDeletionOptions=%+v", s.Namespace, ing.Name, options)
