4186603c3abf7127afc1d0011b9f63b9e3d8ab46
David Cheung <cheungdavid@google.com>
2024-05-22 06:33:35 +0000

    Mark Pods from non-default subnet as ready.

--- pkg/neg/readiness/utils_test.go
+++ pkg/neg/readiness/utils_test.go
@@ -587,9 +587,6 @@ func generatePod(namespace, name string, hasNegReadinessGate, hasNegCondition, n
 		Spec: v1.PodSpec{
 			NodeName: "instance1",
 		},
-		Spec: v1.PodSpec{
-			NodeName: "instance1",
-		},
 	}
 	if hasNegReadinessGate {
 		ret.Spec.ReadinessGates = []v1.PodReadinessGate{{ConditionType: shared.NegReadinessGate}}
