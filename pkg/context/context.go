3e11be682e0827a8dc298c24ccbfb242e55ad8f1
maciejriedl <maciejriedl@google.com>
2024-05-23 12:45:23 +0000

    Updates version of 'informerfirewall' from v1beta to v1.

--- pkg/context/context.go
+++ pkg/context/context.go
@@ -34,7 +34,7 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 	firewallclient "k8s.io/cloud-provider-gcp/crd/client/gcpfirewall/clientset/versioned"
-	informerfirewall "k8s.io/cloud-provider-gcp/crd/client/gcpfirewall/informers/externalversions/gcpfirewall/v1beta1"
+	informerfirewall "k8s.io/cloud-provider-gcp/crd/client/gcpfirewall/informers/externalversions/gcpfirewall/v1"
 	networkclient "k8s.io/cloud-provider-gcp/crd/client/network/clientset/versioned"
 	informernetwork "k8s.io/cloud-provider-gcp/crd/client/network/informers/externalversions/network/v1"
 	"k8s.io/cloud-provider-gcp/providers/gce"
