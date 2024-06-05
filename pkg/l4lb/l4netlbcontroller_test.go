bbefb0198b88dfb72c2335192448ba1efe135cec
Cezary Zawadka <czawadka@google.com>
2024-06-05 17:53:26 +0200

    fix instance group sizes
    there is an error in truncating to 1000 nodes before iterating over zones when adding/removing instances from instance group.
    
    The instance group management logic should be following:
    
    - For each zone add up to #m.maxIGSize number of nodes to the instance group
    - If there is more then truncate last nodes (in alphabetical order)
    - the logic should be consistent with cloud-provider-gcp's Legacy L4 ILB Controller: https://github.com/kubernetes/cloud-provider-gcp/blob/fca628cb3bf9267def0abb509eaae87d2d4040f3/providers/gce/gce_loadbalancer_internal.go#L606C1-L675C1
    - the  production value for m.maxIGSize should be set to 1000 as is in the cloud-provider-gcp.

--- pkg/l4lb/l4netlbcontroller_test.go
+++ pkg/l4lb/l4netlbcontroller_test.go
@@ -770,17 +770,23 @@ func TestProcessServiceCreationFailed(t *testing.T) {
 	for _, param := range []struct {
 		addMockFunc   func(*cloud.MockGCE)
 		expectedError string
-	}{{addMockFunc: func(c *cloud.MockGCE) { c.MockInstanceGroups.GetHook = test.GetErrorInstanceGroupHook },
-		expectedError: "lc.instancePool.EnsureInstanceGroupsAndPorts(k8s-ig--aaaaa, []) returned error GetErrorInstanceGroupHook"},
-		{addMockFunc: func(c *cloud.MockGCE) { c.MockInstanceGroups.ListHook = test.ListErrorHook },
-			expectedError: "ListErrorHook"},
-		{addMockFunc: func(c *cloud.MockGCE) { c.MockInstanceGroups.InsertHook = test.InsertErrorHook },
-			expectedError: "lc.instancePool.EnsureInstanceGroupsAndPorts(k8s-ig--aaaaa, []) returned error InsertErrorHook"},
-
-		{addMockFunc: func(c *cloud.MockGCE) { c.MockInstanceGroups.AddInstancesHook = test.AddInstancesErrorHook },
-			expectedError: "AddInstances: [AddInstancesErrorHook]"},
-		{addMockFunc: func(c *cloud.MockGCE) { c.MockInstanceGroups.ListInstancesHook = test.ListInstancesWithErrorHook },
-			expectedError: "ListInstancesWithErrorHook"},
+	}{
+		{
+			addMockFunc:   func(c *cloud.MockGCE) { c.MockInstanceGroups.GetHook = test.GetErrorInstanceGroupHook },
+			expectedError: "lc.instancePool.EnsureInstanceGroupsAndPorts(k8s-ig--aaaaa, []) returned error GetErrorInstanceGroupHook",
+		},
+		{
+			addMockFunc:   func(c *cloud.MockGCE) { c.MockInstanceGroups.InsertHook = test.InsertErrorHook },
+			expectedError: "lc.instancePool.EnsureInstanceGroupsAndPorts(k8s-ig--aaaaa, []) returned error InsertErrorHook",
+		},
+		{
+			addMockFunc:   func(c *cloud.MockGCE) { c.MockInstanceGroups.AddInstancesHook = test.AddInstancesErrorHook },
+			expectedError: "AddInstancesErrorHook",
+		},
+		{
+			addMockFunc:   func(c *cloud.MockGCE) { c.MockInstanceGroups.ListInstancesHook = test.ListInstancesWithErrorHook },
+			expectedError: "ListInstancesWithErrorHook",
+		},
 	} {
 		lc := newL4NetLBServiceController()
 		param.addMockFunc((lc.ctx.Cloud.Compute().(*cloud.MockGCE)))
@@ -789,7 +795,7 @@ func TestProcessServiceCreationFailed(t *testing.T) {
 		key, _ := common.KeyFunc(svc)
 		err := lc.sync(key, klog.TODO())
 		if err == nil || err.Error() != param.expectedError {
-			t.Errorf("Error mismatch '%v' != '%v'", err, param.expectedError)
+			t.Errorf("Error mismatch got:'%v' want: '%v'", err, param.expectedError)
 		}
 	}
 }
