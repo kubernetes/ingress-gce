c3cbc969b299ecb1d98e5f7235589be4fa0c6c5d
Slavik Panasovets <slavik@google.com>
2025-01-11 19:23:30 +0000

    Remove multiproject sharedcontext. It was a super structure which didnt have specific purpose

--- pkg/multiproject/start/start.go
+++ pkg/multiproject/start/start.go
@@ -2,14 +2,20 @@ package start
 
 import (
 	"context"
+	"encoding/json"
+	"os"
 
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/ingress-gce/cmd/glbc/app"
+	ingresscontext "k8s.io/ingress-gce/pkg/context"
 	"k8s.io/ingress-gce/pkg/flags"
+	_ "k8s.io/ingress-gce/pkg/klog"
 	pccontroller "k8s.io/ingress-gce/pkg/multiproject/controller"
 	"k8s.io/ingress-gce/pkg/multiproject/manager"
-	"k8s.io/ingress-gce/pkg/multiproject/sharedcontext"
+	"k8s.io/ingress-gce/pkg/neg/syncers/labels"
 	providerconfigclient "k8s.io/ingress-gce/pkg/providerconfig/client/clientset/versioned"
 	providerconfiginformers "k8s.io/ingress-gce/pkg/providerconfig/client/informers/externalversions"
 	svcnegclient "k8s.io/ingress-gce/pkg/svcneg/client/clientset/versioned"
@@ -25,37 +31,49 @@ func Start(
 	svcNegClient svcnegclient.Interface,
 	kubeSystemUID types.UID,
 	eventRecorderKubeClient kubernetes.Interface,
-	namer *namer.Namer,
+	rootNamer *namer.Namer,
 	stopCh <-chan struct{},
 ) {
 	providerConfigClient, err := providerconfigclient.NewForConfig(kubeConfig)
 	if err != nil {
 		klog.Fatalf("Failed to create ProviderConfig client: %v", err)
 	}
-	sharedContext := sharedcontext.NewSharedContext(
-		kubeClient,
-		svcNegClient,
-		kubeSystemUID,
-		eventRecorderKubeClient,
-		namer,
-		logger,
-		stopCh,
-	)
+
+	lpConfig := labels.PodLabelPropagationConfig{}
+	if flags.F.EnableNEGLabelPropagation {
+		lpConfigEnvVar := os.Getenv("LABEL_PROPAGATION_CONFIG")
+		if err := json.Unmarshal([]byte(lpConfigEnvVar), &lpConfig); err != nil {
+			logger.Error(err, "Failed to retrieve pod label propagation config")
+		}
+	}
+
+	ctxConfig := ingresscontext.ControllerContextConfig{
+		ResyncPeriod: flags.F.ResyncPeriod,
+	}
+
+	defaultGCEConfig, err := app.GCEConfString(logger)
+	if err != nil {
+		klog.Fatalf("Error getting default cluster GCE config: %v", err)
+	}
+
+	informersFactory := informers.NewSharedInformerFactoryWithOptions(kubeClient, ctxConfig.ResyncPeriod)
+
+	providerConfigInformer := providerconfiginformers.NewSharedInformerFactory(providerConfigClient, ctxConfig.ResyncPeriod).Providerconfig().V1().ProviderConfigs().Informer()
+
 	manager := manager.NewProviderConfigControllerManager(
 		kubeClient,
-		sharedContext.InformersFactory,
+		informersFactory,
 		providerConfigClient,
-		sharedContext.SvcNegClient,
-		sharedContext.EventRecorderClient,
-		sharedContext.KubeSystemUID,
-		sharedContext.ClusterNamer,
-		sharedContext.L4Namer,
-		sharedContext.LpConfig,
-		sharedContext.DefaultCloudConfig,
-		sharedContext.GlobalStopCh,
+		svcNegClient,
+		eventRecorderKubeClient,
+		kubeSystemUID,
+		rootNamer,
+		namer.NewL4Namer(string(kubeSystemUID), rootNamer),
+		lpConfig,
+		defaultGCEConfig,
+		stopCh,
 		logger,
 	)
-	providerConfigInformer := providerconfiginformers.NewSharedInformerFactory(providerConfigClient, flags.F.ResyncPeriod).Providerconfig().V1().ProviderConfigs().Informer()
 
 	pcController := pccontroller.NewProviderConfigController(manager, providerConfigInformer, stopCh, logger)
 
