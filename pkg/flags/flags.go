566b70c1bd9fe769f663ffad8e069a73a5627274
Slavik Panasovets <slavik@google.com>
2025-05-13 22:13:46 +0000

    Allow ability to override firewall source ranges

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -143,6 +143,7 @@ var F = struct {
 	EnableL4NetLBForwardingRulesOptimizations bool
 	EnableIPV6OnlyNEG                         bool
 	MultiProjectOwnerLabelKey                 string
+	OverrideHealthCheckSourceCIDRs            string
 	EnableL4LBLoggingAnnotations              bool
 	OverrideHealthCheckSourceCIDRs            string
 
