49900771f947df183fb04b5544844f7a08223a9f
Slavik Panasovets <slavik@google.com>
2025-05-28 18:53:07 +0000

    Rename Deprecated flag vars to prefix with Deprecated

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -147,13 +147,13 @@ var F = struct {
 	// DEPRECATED FLAGS
 	// ===============================
 	// DEPRECATED: ASM ConfigMap based config is no longer used and will be removed in a future release.
-	EnableASMConfigMapBasedConfig    bool
-	ASMConfigMapBasedConfigCMName    string
-	ASMConfigMapBasedConfigNamespace string
-	// DEPRECATED: EnableDeleteUnusedFrontends is on by default and will be removed in a future release.
-	EnableDeleteUnusedFrontends bool
-	// DEPRECATED: EnableFrontendConfig is on by default and will be removed in a future release.
-	EnableFrontendConfig bool
+	DeprecatedEnableASMConfigMapBasedConfig    bool
+	DeprecatedASMConfigMapBasedConfigCMName    string
+	DeprecatedASMConfigMapBasedConfigNamespace string
+	// DEPRECATED: DeprecatedEnableDeleteUnusedFrontends is on by default and will be removed in a future release.
+	DeprecatedEnableDeleteUnusedFrontends bool
+	// DEPRECATED: DeprecatedEnableFrontendConfig is on by default and will be removed in a future release.
+	DeprecatedEnableFrontendConfig bool
 }{
 	GCERateLimitScale: 1.0,
 }
@@ -215,7 +215,7 @@ form namespace/name.`)
 	flag.StringVar(&F.DefaultSvcPortName, "default-backend-service-port", "http",
 		`Specify the default service's port used to serve a 404 page for the default backend. Takes
 only the port's name - not its number.`)
-	flag.BoolVar(&F.EnableFrontendConfig, "enable-frontend-config", true,
+	flag.BoolVar(&F.DeprecatedEnableFrontendConfig, "enable-frontend-config", true,
 		`DEPCREATED: Optional, whether or not to enable FrontendConfig.`)
 	flag.Var(&F.GCERateLimit, "gce-ratelimit",
 		`Optional, can be used to rate limit certain GCE API calls. Example usage:
@@ -285,11 +285,11 @@ L7 load balancing. CSV values accepted. Example: -node-port-ranges=80,8080,400-5
 		F.FinalizerAdd, "Enable adding Finalizer to Ingress.")
 	flag.BoolVar(&F.FinalizerRemove, "enable-finalizer-remove",
 		F.FinalizerRemove, "Enable removing Finalizer from Ingress.")
-	flag.BoolVar(&F.EnableASMConfigMapBasedConfig, "enable-asm-config-map-config", false, "DEPRECATED: Enable ASMConfigMapBasedConfig")
-	flag.StringVar(&F.ASMConfigMapBasedConfigNamespace, "asm-configmap-based-config-namespace", "kube-system", "DEPRECATED: ASM Configmap based config: configmap namespace")
-	flag.StringVar(&F.ASMConfigMapBasedConfigCMName, "asm-configmap-based-config-cmname", "ingress-controller-asm-cm-config", "DEPRECATED: ASM Configmap based config: configmap name")
+	flag.BoolVar(&F.DeprecatedEnableASMConfigMapBasedConfig, "enable-asm-config-map-config", false, "DEPRECATED: Enable ASMConfigMapBasedConfig")
+	flag.StringVar(&F.DeprecatedASMConfigMapBasedConfigNamespace, "asm-configmap-based-config-namespace", "kube-system", "DEPRECATED: ASM Configmap based config: configmap namespace")
+	flag.StringVar(&F.DeprecatedASMConfigMapBasedConfigCMName, "asm-configmap-based-config-cmname", "ingress-controller-asm-cm-config", "DEPRECATED: ASM Configmap based config: configmap name")
 	flag.BoolVar(&F.EnableNonGCPMode, "enable-non-gcp-mode", false, "Set to true when running on a non-GCP cluster.")
-	flag.BoolVar(&F.EnableDeleteUnusedFrontends, "enable-delete-unused-frontends", true, "DEPRECATED: Enable deleting unused gce frontend resources.")
+	flag.BoolVar(&F.DeprecatedEnableDeleteUnusedFrontends, "enable-delete-unused-frontends", true, "DEPRECATED: Enable deleting unused gce frontend resources.")
 	flag.BoolVar(&F.EnableV2FrontendNamer, "enable-v2-frontend-namer", false, "Enable v2 ingress frontend naming policy.")
 	flag.BoolVar(&F.RunIngressController, "run-ingress-controller", true, `Optional, if enabled then the ingress controller will be run.`)
 	flag.BoolVar(&F.RunL4Controller, "run-l4-controller", false, `Optional, whether or not to run L4 Service Controller as part of glbc. If set to true, services of Type:LoadBalancer with Internal annotation will be processed by this controller.`)
