1a58f127146b280e21a5c79a2a830e781ca699dd
Zuzanna Nowak <zuzannanowak@google.com>
2025-08-26 12:21:17 +0000

    Add --enable-l4-ipv6-only-mode flag
    Add --enable-l4-ipv6-only-mode flag
    
    Rename EnableL4IPv6OnlyMode flag to EnableIPv6OnlyL4
    
    Run gofmt on flags.go

--- pkg/flags/flags.go
+++ pkg/flags/flags.go
@@ -146,6 +146,7 @@ var F = struct {
 	MultiProjectOwnerLabelKey                 string
 	OverrideHealthCheckSourceCIDRs            string
 	ManageL4LBLogging                         bool
+	EnableIPv6OnlyL4                          bool
 
 	// ===============================
 	// DEPRECATED FLAGS
@@ -356,6 +357,7 @@ L7 load balancing. CSV values accepted. Example: -node-port-ranges=80,8080,400-5
 	flag.StringVar(&F.OverrideHealthCheckSourceCIDRs, "override-health-check-src-cidrs", "", "Overrides the default source IP ranges used when configuring firewall rules to allow health check probes for L7 load balancers. Provide the ranges as a comma-separated list of CIDRs. Example: --override-health-check-src-cidrs=130.211.0.0/22,35.191.0.0/16")
 	flag.BoolVar(&F.ManageL4LBLogging, "manage-l4lb-logging", false, "Manage L4 ILB/NetLB logging.")
 	flag.BoolVar(&F.ReadOnlyMode, "read-only-controllers", false, "When enabled, this flag runs the IG, NEG, L4 ILB, and L4 NetLB controllers in a read-only mode. This prevents them from executing any mutating API calls (e.g., create, update, delete), allowing you to safely observe controller behavior without modifying resources. The Ingress controller is exempt from this mode.")
+	flag.BoolVar(&F.EnableIPv6OnlyL4, "enable-ipv6-only-l4", false, "Enables IPv6-only mode for the L4 ILB and NetLB controllers, disabling all IPv4-related logic and resource management.")
 }
 
 func Validate() {
